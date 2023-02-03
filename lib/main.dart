eb58763c8330d71d2f923f01d9f5a1d1198d058e
alexqgon93 <alexqgon93@gmail.com>
2023-02-03 09:51:01 +0100

    Added new screens and new routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_components/screens/listview1_screen.dart';
+import 'package:flutter_components/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,9 +8,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        home: Listview1Screen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'listView1': (context) => const Listview1Screen(),
+        'listView2': (context) => const Listview2Screen(),
+        'alert': (context) => const AlertScreen(),
+        'card': (context) => const CardScreen(),
+      },
+    );
   }
 }
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  final options = const ['Megaman', 'Luigi', 'Super Smash', 'Mario'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Listiew2 TabBar'),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: ListView.separated(
+          itemCount: options.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(options[index]),
+            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+            onTap: () {
+              print(options[index]);
+            },
+          ),
+          separatorBuilder: (context, index) => const Divider(),
+          /*children: [
+            ...options
+                .map((game) => ListTile(
+                      title: Text(game),
+                      trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                    ))
+                .toList(),
+            const Divider(),
+          ],*/
+        ));
+  }
+}
