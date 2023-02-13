fc3d977e6a62c6edfe266e0358c029f4f7c82f74
alexqgon93 <alexqgon93@gmail.com>
2023-02-13 17:42:50 +0100

    Added new alert dialog onpressed and new elevated button apptheme

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,11 +3,53 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 5,
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100)
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'))
+            ],
+            title: const Text('Title'),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alerts Screen'),
+      ),
       body: Center(
-        child: Text('AlertScreen'),
+        child: ElevatedButton(
+            onPressed: () => displayDialog(context),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Text(
+                'Mostrar Alerta',
+                style: TextStyle(fontSize: 16),
+              ),
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.pop(context),
+        child: const Icon(Icons.close),
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,6 +5,14 @@ class AppTheme {
   static final ThemeData lightTheme = ThemeData.light().copyWith(
       primaryColor: Colors.indigo,
       appBarTheme: const AppBarTheme(color: primary),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: AppTheme.primary,
+            shape: const StadiumBorder(),
+            elevation: 10),
+      ),
+      floatingActionButtonTheme:
+          const FloatingActionButtonThemeData(backgroundColor: primary),
       textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(foregroundColor: primary)));
 
